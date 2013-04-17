495390de2ba7c2348cb319063d16802b8eff7844
val100 <valery@agrologic.com>
2013-04-17 11:13:14 +0300

    Moved keep alive logic int KeepAliveMessage class and added test

--- src/main/java/com/agrologic/app/messaging/KeepAliveMessage.java
+++ src/main/java/com/agrologic/app/messaging/KeepAliveMessage.java
@@ -6,12 +6,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class KeepAliveMessage {
+
+    public static final String BUUFER_IS_NULL_MSG = "Buffer must not be null";
+    public static final String WRONG_FORMAT_MSG = "Message does not match the required format";
     private final String username;
     private final String password;
     private final String version;
 
-
-    public KeepAliveMessage( String username, String password, String version) {
+    public KeepAliveMessage(String username, String password, String version) {
         this.version = version;
         this.password = password;
         this.username = username;
@@ -29,27 +31,37 @@ public class KeepAliveMessage {
         return username;
     }
 
-    public static KeepAliveMessage parseIncomingBytes(byte[] payload) throws WrongMessageFormatException {
+    public static KeepAliveMessage parseIncomingBytes(byte[] payload) throws WrongMessageFormatException,
+            NullPointerException {
+        if (payload == null) {
+            throw new NullPointerException(BUUFER_IS_NULL_MSG);
+        }
         int stx = ByteUtil.indexOf(payload, Message.STX);
         int etx = ByteUtil.indexOf(payload, Message.ETX);
         if (stx < 0 || etx < 0) {
-            throw new WrongMessageFormatException("Keep alive of wrong format came");
+            throw new WrongMessageFormatException(WRONG_FORMAT_MSG);
         }
         byte[] data = Arrays.copyOfRange(payload, stx + 1, etx);
         List<byte[]> dataList = ByteUtil.split(data, Message.RS);
 
+        if (dataList.size() < 1) {
+            throw new WrongMessageFormatException(WRONG_FORMAT_MSG);
+        }
+
         int PASS_INDEX = 0;
         int NAME_INDEX = 1;
         int VERS_INDEX = 2;
+        String psswd, name, vers = "N/A";;
         data = dataList.get(PASS_INDEX);
-        String psswd = new String(data, 0, data.length);
+        psswd = new String(data, 0, data.length);
         data = dataList.get(NAME_INDEX);
-        String name = new String(data, 0, data.length);
-        String vers = "N/A";
+        name = new String(data, 0, data.length);
+        vers = "N/A";
         if (dataList.size() > VERS_INDEX) {
             data = dataList.get(VERS_INDEX);
             vers = new String(data, 0, data.length);
         }
+
         return new KeepAliveMessage(name, psswd, vers);
     }
 }
--- src/test/java/com/agrologic/app/messaging/KeepAliveMessageTest.java
+++ src/test/java/com/agrologic/app/messaging/KeepAliveMessageTest.java
@@ -1,10 +1,40 @@
 package com.agrologic.app.messaging;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class KeepAliveMessageTest {
+
+    private final static byte[] PAY_LOAD = new byte[]{Message.STX, 'c', 't', 'b',
+        Message.RS, 'c', 't', 'b',
+        Message.RS, '5', '.', '0', '1', 'W', 'e', '*',
+        Message.RS, Message.ETX};
+    private final static byte[] WRONG_FORMAT_PAY_LOAD = new byte[]{Message.STX, 'c', 't', 'b', 'c', 't', 'b', Message.ETX};
+
     @Test
     public void testParseIncomingBytes() throws Exception {
+        KeepAliveMessage kam = KeepAliveMessage.parseIncomingBytes(PAY_LOAD);
+        assertEquals(kam.getUsername(), ("ctb"));
+        assertEquals(kam.getPassword(), ("ctb"));
+        assertEquals(kam.getVersion(), ("5.01We*"));
+    }
 
+    @Test
+    public void testParseIncomingBytesIfBufferIsNull() throws WrongMessageFormatException {
+        try {
+            KeepAliveMessage.parseIncomingBytes(null);
+        } catch (NullPointerException e) {
+            assert (e.getMessage().equals(KeepAliveMessage.BUUFER_IS_NULL_MSG));
+        }
+    }
+
+    @Test
+    public void testParseIncomingBytesIfFormatIsWrong() {
+        try {
+            KeepAliveMessage.parseIncomingBytes(WRONG_FORMAT_PAY_LOAD);
+        } catch (WrongMessageFormatException e) {
+            assert (e.getMessage().equals(KeepAliveMessage.WRONG_FORMAT_MSG));
+        }
     }
 }
