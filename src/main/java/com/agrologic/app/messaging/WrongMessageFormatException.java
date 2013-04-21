67197cacac6ae1bfb9499d48dac01c5b47070791
val100 <valery@agrologic.com>
2013-04-21 11:42:10 +0300

    removed sources from pom module

--- src/main/java/com/agrologic/app/messaging/WrongMessageFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.agrologic.app.messaging;
-
-public class WrongMessageFormatException extends Exception {
-    public WrongMessageFormatException(String message) {
-        super(message);
-    }
-}
