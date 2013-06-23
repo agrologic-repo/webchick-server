026a7e53475c6ab3063f56e8146469f87b90f6ff
val100 <valery@agrologic.com>
2013-06-23 11:54:47 +0300

    - added unit test to RequestMesssageQueue #6

--- webchick-server-main/src/test/java/com/agrologic/app/messaging/RequestMessageQueueTest.java
+++ webchick-server-main/src/test/java/com/agrologic/app/messaging/RequestMessageQueueTest.java
@@ -9,16 +9,18 @@ import java.util.Observable;
 import java.util.Observer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 public class RequestMessageQueueTest implements Observer {
-    RequestMessageQueue sut; //system under test
-    MessageFactory messageFactory;
+    private RequestMessageQueue sut; //system under test
+    private MessageFactory messageFactory;
 
     @Before
     public void setUp() throws Exception {
+        messageFactory = new MessageFactory();
         sut = new RequestMessageQueue();
         sut.addObserver(this);
-        messageFactory = new MessageFactory();
+        sut.notifyToPrepareRequests();
     }
 
     @After
@@ -47,15 +49,41 @@ public class RequestMessageQueueTest implements Observer {
     }
 
     @Test
-    public void notifyToCreateRequestToChange() {
+    public void requestDoesNotExitsInRequestQueueAfterGettingRequest() throws Exception {
+        sut.getRequest(); // skip one request
+        RequestMessage actual = (RequestMessage) sut.getRequest();
+        RequestMessage expected = messageFactory.createRealTimeRequests("T901").get(0);
+        assertNotSame(expected, actual);
+    }
+
+    @Test
+    public void requestToChangeAddedToQueueAndReturnByItPriority() {
         sut.notifyToCreateRequestToChange();
+        RequestMessage actual = (RequestMessage) sut.getRequest();
+        RequestMessage expected = messageFactory.createWriteRequest("T901", 4096L, 255L);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void previousRequestReturnIfNeedToo() {
+        sut.getRequest();// skip request and call it again
+        sut.setReplyForPreviousRequestPending(true);
+        RequestMessage actual = (RequestMessage) sut.getRequest();
+        RequestMessage expected = messageFactory.createRealTimeRequests("T901").get(0);
+        assertEquals(expected, actual);
     }
 
     @Override
     public void update(Observable o, Object arg) {
         CommandType actualCommand = (CommandType) arg;
-        CommandType expectedCommand = sut.getCommandType();
-        sut.addRequest(messageFactory.createRealTimeRequests("T901").get(0));
-        assertEquals(expectedCommand, actualCommand);
+        switch (actualCommand) {
+            case CREATE_REQUEST:
+                sut.addRequest(messageFactory.createRealTimeRequests("T901").get(0));
+                sut.addRequest(messageFactory.createRealTimeRequests("T901").get(1));
+                break;
+            case CREATE_REQUEST_TO_WRITE:
+                sut.addRequest(messageFactory.createWriteRequest("T901", 4096L, 255L));
+                break;
+        }
     }
 }
