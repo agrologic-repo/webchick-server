0749ba1100f25c3c18b1f9d2860d3ae313b59e6b
val100 <valery@agrologic.com>
2013-07-23 11:55:19 +0300

    - moved the whole UserDao under SpringJdbc #21   - changed version   - documented dao interfaces

--- webchick-dao-mysql/src/main/java/com/agrologic/app/dao/mysql/impl/UserDaoImpl.java
+++ webchick-dao-mysql/src/main/java/com/agrologic/app/dao/mysql/impl/UserDaoImpl.java
@@ -2,318 +2,187 @@ package com.agrologic.app.dao.mysql.impl;
 
 import com.agrologic.app.dao.DaoFactory;
 import com.agrologic.app.dao.UserDao;
+import com.agrologic.app.dao.mappers.RowMappers;
 import com.agrologic.app.model.User;
-import com.agrologic.app.dao.mappers.UserUtil;
+import org.apache.commons.lang.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoImpl implements UserDao {
-    protected DaoFactory dao;
 
-    public UserDaoImpl(DaoFactory daoFactory) {
-        dao = daoFactory;
+    protected final DaoFactory dao;
+    private final Logger logger = LoggerFactory.getLogger(AlarmDaoImpl.class);
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
+
+    public UserDaoImpl(JdbcTemplate jdbcTemplate, DaoFactory dao) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        this.jdbcInsert.setTableName(USER_TABLE);
+        this.dao = dao;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void insert(User user) throws SQLException {
-        String sqlQuery =
-                "INSERT INTO USERS (USERID, NAME, PASSWORD, FIRSTNAME, LASTNAME, ROLE, STATE, PHONE, EMAIL, COMPANY)"
-                        + " VALUES (?,?,?,?,?,?,?,?,?,?)";
-        PreparedStatement prepstmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            prepstmt = con.prepareStatement(sqlQuery);
-            prepstmt.setObject(1, null);
-            prepstmt.setString(2, user.getLogin());
-            prepstmt.setString(3, user.getPassword());
-            prepstmt.setString(4, user.getFirstName());
-            prepstmt.setString(5, user.getLastName());
-            prepstmt.setInt(6, user.getUserRole().getValue());
-            prepstmt.setInt(7, user.getState());
-            prepstmt.setString(8, user.getPhone());
-            prepstmt.setString(9, user.getEmail());
-            prepstmt.setString(10, user.getCompany());
-            prepstmt.executeUpdate();
-        } catch (SQLException e) {
-            dao.printSQLException(e);
-
-            throw new SQLException("Cannot Insert New User To The DataBase", e);
-        } finally {
-            prepstmt.close();
-            dao.closeConnection(con);
-        }
+    public void insert(User user) {
+        logger.debug("Creating user with id [{}]", user.getLogin());
+        Map<String, Object> valuesToInsert = new HashMap<String, Object>();
+        valuesToInsert.put("name", user.getLogin());
+        valuesToInsert.put("password", user.getPassword());
+        valuesToInsert.put("firstname", user.getFirstName());
+        valuesToInsert.put("lastname", user.getLastName());
+        valuesToInsert.put("role", user.getRole().getValue());
+        valuesToInsert.put("state", user.getState());
+        valuesToInsert.put("phone", user.getPhone());
+        valuesToInsert.put("email", user.getEmail());
+        valuesToInsert.put("company", user.getCompany());
+        jdbcInsert.execute(valuesToInsert);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void update(User user) throws SQLException {
-        String sqlQuery =
-                "update users set Name=?,Password=?,FirstName=?,LastName=?,Role=?,Phone=?,Email=?, Company=? where UserID=?";
-        PreparedStatement prepstmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            prepstmt = con.prepareStatement(sqlQuery);
-            prepstmt.setString(1, user.getLogin());
-            prepstmt.setString(2, user.getPassword());
-            prepstmt.setString(3, user.getFirstName());
-            prepstmt.setString(4, user.getLastName());
-            prepstmt.setInt(5, user.getUserRole().getValue());
-            prepstmt.setString(6, user.getPhone());
-            prepstmt.setString(7, user.getEmail());
-            prepstmt.setString(8, user.getCompany());
-            prepstmt.setLong(9, user.getId());
-            prepstmt.executeUpdate();
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Update User In DataBase", e);
-        } finally {
-            prepstmt.close();
-            dao.closeConnection(con);
-        }
+    public void update(User user) {
+        logger.debug("Update user with id [{}]", user.getId());
+        jdbcTemplate.update("update users set Name=?,Password=?,FirstName=?,LastName=?,Role=?,Phone=?,Email=?, " +
+                "Company=? where UserID=?",
+                new Object[]{user.getLogin(), user.getPassword(), user.getFirstName(), user.getLastName(),
+                        user.getRole(), user.getPhone(), user.getEmail(), user.getCompany(), user.getId()});
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void remove(Long userId) throws SQLException {
-        String sqlQuery = "delete from users where UserID=?";
-        PreparedStatement prepstmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            prepstmt = con.prepareStatement(sqlQuery);
-            prepstmt.setLong(1, userId);
-            prepstmt.executeUpdate();
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Delete User From DataBase", e);
-        } finally {
-            prepstmt.close();
-            dao.closeConnection(con);
-        }
+    public void remove(Long id) throws SQLException {
+        Validate.notNull(id, "Id can not be null");
+        logger.debug("delete from users where UserID=?", id);
+        jdbcTemplate.update("delete from users where userID=?", new Object[]{id});
     }
 
+//    @Override
+//    public Integer getTotalNumUsers() throws SQLException {
+//        String sqlQuery = "select count(*) as num from users";
+//        Statement stmt = null;
+//        Connection con = null;
+//
+//        try {
+//            con = dao.getConnection();
+//            stmt = con.createStatement();
+//
+//            ResultSet rs = stmt.executeQuery(sqlQuery);
+//
+//            if (rs.next()) {
+//                return rs.getInt("num");
+//            } else {
+//                return 0;
+//            }
+//        } catch (SQLException e) {
+//            dao.getConnection();
+//
+//            throw new SQLException("Cannot Retrieve Users From DataBase", e);
+//        } finally {
+//            stmt.close();
+//            dao.closeConnection(con);
+//        }
+//    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public Integer getTotalNumUsers() throws SQLException {
-        String sqlQuery = "select count(*) as num from users";
-        Statement stmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            stmt = con.createStatement();
-
-            ResultSet rs = stmt.executeQuery(sqlQuery);
-
-            if (rs.next()) {
-                return rs.getInt("num");
-            } else {
-                return 0;
-            }
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Retrieve Users From DataBase", e);
-        } finally {
-            stmt.close();
-            dao.closeConnection(con);
+    public User getById(Long id) throws SQLException {
+        logger.debug("Get user with id [{}]", id);
+        String sqlQuery = "select * from users where UserID=?";
+        List<User> users = jdbcTemplate.query(sqlQuery, new Object[]{id}, RowMappers.user());
+        if (users.isEmpty()) {
+            return null;
         }
+        return users.get(0);
     }
 
-    @Override
-    public User getById(Long userId) throws SQLException {
-        String sqlQuery = "select * from users where UserID=" + userId;
-        Statement stmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            stmt = con.createStatement();
-
-            ResultSet rs = stmt.executeQuery(sqlQuery);
-
-            if (rs.next()) {
-                return UserUtil.makeUser(rs);
-            } else {
-                return new User();
-            }
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Retrieve User By id From DataBase", e);
-        } finally {
-            stmt.close();
-            dao.closeConnection(con);
-        }
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public User validate(String loginName, String loginPassword) throws SQLException {
+        logger.debug("Validation user name and password ");
         String sqlQuery = "select * from users where Name = ? and Password = ?";
-        PreparedStatement prepstmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            prepstmt = con.prepareStatement(sqlQuery);
-            prepstmt.setString(1, loginName);
-            prepstmt.setString(2, loginPassword);
-
-            ResultSet rs = prepstmt.executeQuery();
-
-            if (rs.next()) {
-                return UserUtil.makeUser(rs);
-            } else {
-                return new User();
-            }
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Validate User In DataBase ", e);
-        } finally {
-            prepstmt.close();
-            dao.closeConnection(con);
+        List<User> users = jdbcTemplate.query(sqlQuery, new Object[]{loginName, loginPassword}, RowMappers.user());
+        if (users.isEmpty()) {
+            return new User();
         }
+        return users.get(0);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public Boolean loginEnabled(String login) throws SQLException {
+    public Boolean loginEnabled(String login) {
+        logger.debug("Check if user login already exist ");
         String sqlQuery = "select * from users where Name = ?";
-        PreparedStatement prepstmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            prepstmt = con.prepareStatement(sqlQuery);
-            prepstmt.setString(1, login);
-
-            ResultSet rs = prepstmt.executeQuery();
-
-            return rs.next();
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Validate User In DataBase", e);
-        } finally {
-            prepstmt.close();
-            dao.closeConnection(con);
-        }
+        List<User> users = jdbcTemplate.query(sqlQuery, new Object[]{login}, RowMappers.user());
+        return users.isEmpty() ? true : false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Collection<String> getUserCompanies() throws SQLException {
+        logger.debug("Get all users companies ");
         String sqlQuery = "select distinct company from users";
-        Statement stmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            stmt = con.createStatement();
-
-            ResultSet rs = stmt.executeQuery(sqlQuery);
-            List<String> companies = new ArrayList<String>();
-
-            while (rs.next()) {
-                companies.add(rs.getString("Company"));
+        return jdbcTemplate.query(sqlQuery, new ParameterizedRowMapper<String>() {
+            public String mapRow(ResultSet rs, int arg1) throws SQLException {
+                return rs.getString(1);
             }
-
-            return companies;
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Retrieve Companies From DataBase", e);
-        } finally {
-            stmt.close();
-            dao.closeConnection(con);
-        }
+        });
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public Collection<User> getAll() throws SQLException {
+    public Collection<User> getAll() {
+        logger.debug("Get all users ");
         String sqlQuery = "select * from users";
-        Statement stmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            stmt = con.createStatement();
-
-            ResultSet rs = stmt.executeQuery(sqlQuery);
-
-            return UserUtil.makeUserList(rs);
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Retrieve Users From DataBase", e);
-        } finally {
-            stmt.close();
-            dao.closeConnection(con);
-        }
+        return jdbcTemplate.query(sqlQuery, RowMappers.user());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Collection<User> getAllByRole(Integer role) throws SQLException {
+        logger.debug("Get all users by role ");
         String sqlQuery = "select * from users where Role =?";
-        PreparedStatement prepstmt = null;
-        Connection con = null;
-
-        try {
-            con = dao.getConnection();
-            prepstmt = con.prepareStatement(sqlQuery);
-            prepstmt.setInt(1, role);
-
-            ResultSet rs = prepstmt.executeQuery();
-
-            return UserUtil.makeUserList(rs);
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Retrieve Users From DataBase", e);
-        } finally {
-            prepstmt.close();
-            dao.closeConnection(con);
-        }
+        return jdbcTemplate.query(sqlQuery, new Object[]{role}, RowMappers.user());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public Collection<User> getAll(Integer role, String company, String searchText) throws SQLException {
-        String sqlQuery = "select * from users";
-        Statement stmt = null;
-        Connection con = null;
-
-        if (role != 0) {
-            sqlQuery = "select * from (" + sqlQuery + ") as a where role=" + role;
-        }
-
-        if (!company.equals("All")) {
-            sqlQuery = "select * from (" + sqlQuery + ") as b where company='" + company + "'";
-        }
-
-        if (!company.equals("")) {
-            sqlQuery = "select * from (" + sqlQuery + ") as c where name like '%" + searchText + "%'";
-        }
-
-        try {
-            con = dao.getConnection();
-            stmt = con.createStatement();
-
-            ResultSet rs = stmt.executeQuery(sqlQuery);
-
-            return UserUtil.makeUserList(rs);
-        } catch (SQLException e) {
-            dao.getConnection();
-
-            throw new SQLException("Cannot Retrieve Company From DataBase", e);
-        } finally {
-            stmt.close();
-            dao.closeConnection(con);
-        }
+    public Collection<User> getAll(Integer role, String company, String search) throws SQLException {
+        String sqlQuery = "select * from users where (role=? or ? is null) and (company = ? or ? is null)" +
+                "and name like ? ";
+        logger.debug("Get all users by role ");
+        return jdbcTemplate.query(sqlQuery, new Object[]{role, role, company, company, (search == null ?
+                "%%" : "%" + search + "%")},
+                RowMappers.user());
     }
 }
