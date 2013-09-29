bb5b5c282cdb2a5ff9ba6c171af3087cb4b22d7e
val100 <valery@agrologic.com>
2013-09-29 14:52:40 +0300

    -revision -

--- webchick-server-main/src/main/java/com/agrologic/app/network/NetworkState.java
+++ webchick-server-main/src/main/java/com/agrologic/app/network/NetworkState.java
@@ -1,24 +1,13 @@
-
-/*
-* To change this template, choose Tools | Templates
-* and open the template in the editor.
- */
 package com.agrologic.app.network;
 
-/**
- * Title: ServerNetState <br>
- * Description: <br>
- * Copyright:   Copyright (c) 2009 <br>
- * Company:     AgroLogic LTD. <br>
- * @author      Valery Manakhimov <br>
- * @version     1.1 <br>
- */
 public enum NetworkState {
 
-    /** waiting keep alive */
+    /**
+     * waiting keep alive
+     */
     WAIT_KEEP_ALIVE, STATE_ACCEPT_KEEP_ALIVE, STATE_KEEP_ALIVE_TIMEOUT, STATE_STARTING, STATE_SEND, STATE_BUSY,
-                     STATE_TIMEOUT, STATE_DELAY, STATE_STOP, STATE_RESTART, STATE_ABORT, STATE_IDLE, STATE_READ,
-                     STATE_ERROR
+    STATE_TIMEOUT, STATE_DELAY, STATE_STOP, STATE_RESTART, STATE_ABORT, STATE_IDLE, STATE_READ,
+    STATE_ERROR
 }
 
 
