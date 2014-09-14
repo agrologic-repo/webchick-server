ee51ed09e17d46f10bd6ef116b640ac483f090e6
ctapobep <ctapobep@javatalks.ru>
2014-09-14 15:14:11 +0400

    Added `@Autowired` to the Controller
    Also, now the contexts are loaded when the web app is loaded (added
    listener to web.xml).

--- webchick-web/src/main/java/com/agrologic/app/web/controller/RemoteMainScreenController.java
+++ webchick-web/src/main/java/com/agrologic/app/web/controller/RemoteMainScreenController.java
@@ -1,11 +1,10 @@
 package com.agrologic.app.web.controller;
 
 import com.agrologic.app.dao.CellinkDao;
-import com.agrologic.app.dao.DaoType;
-import com.agrologic.app.dao.DbImplDecider;
 import com.agrologic.app.model.Cellink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,17 +18,20 @@ import java.util.Map;
 @Controller
 public class RemoteMainScreenController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final CellinkDao cellinkDao;
+
+    @Autowired
+    public RemoteMainScreenController(CellinkDao cellinkDao) {
+        this.cellinkDao = cellinkDao;
+    }
 
     @RequestMapping(value = "/rmctrl-main-screen.html", method = RequestMethod.GET)
     public ModelAndView showRemoteMainScreen(@RequestParam(value = "userId") long userId, @RequestParam(value = "cellinkId") long cellinkId) {
         Cellink cellink = null;
         try {
-            CellinkDao cellinkDao = DbImplDecider.use(DaoType.MYSQL).getDao(CellinkDao.class);
             cellink = cellinkDao.getById(cellinkId);
         } catch (SQLException ex) {
             logger.error("Database error", ex);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
         }
 
         Map<String, Object> pageModel = new HashMap<String, Object>();
